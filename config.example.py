8328d965fbcd8459d063ea3fea530e7165c42e56
koksalilayda48-hue <koksalilayda48@gmail.com>
2026-03-30 21:55:54 +0300

    Create config.example.py

---
+++ config.example.py
@@ -0,0 +1,12 @@
+BOT_TOKEN = "8700933139:AAGvhWXesYhP2ECJ7WsrPRPzDJ4B3jOc3BU"
+CHAT_ID = "@anlikhabertrt"
+ADMIN_ID = 6179118477  # Telegram ID'n
+RSS_URLS = [
+    "https://www.trtspor.com.tr/rss.xml",
+    "https://news.google.com/rss?hl=tr&gl=TR&ceid=TR:tr"
+]
+FALLBACK_IMAGES = [
+    "https://via.placeholder.com/600x400?text=SON+DAKIKA",
+    "https://via.placeholder.com/600x400?text=SPOR+HABER",
+    "https://via.placeholder.com/600x400?text=GUNDEME+BOMBA"
+]
